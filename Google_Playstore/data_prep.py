511758c7d5006c7f641fcc6c9ede71eb0e3c85f7
Timon Mwatela <93420789+Mwatim@users.noreply.github.com>
2023-12-28 13:58:52 +0300

    Updates the reliability of the code

--- Google_Playstore/data_prep.py
+++ Google_Playstore/data_prep.py
@@ -1,31 +1,51 @@
 # import requisite data processing packages
+import logging
 import pandas as pd
 
 def data_prep(data):
     """
-    Function to clean and prepare the dataset for the analysis process
+    Function to clean and prepare the dataset for the analysis process.
+
+    Parameters:
+    - data (str): Path to the CSV file.
+
+    Returns:
+    - pd.DataFrame: Cleaned DataFrame.
+    - str: Success message.
     """
-    # read the csv file into a dataframe
-    df = pd.read_csv(data)
+    try:
+        # read the csv file into a dataframe
+        df = pd.read_csv(data)
 
-    # clean the Installs column in prep for analysis 
-    df['Installs'] = df['Installs'].str.replace('[^\d]', '', regex=True)
+        # clean the Installs column in prep for analysis 
+        df['Installs'] = df['Installs'].str.replace('[^\d]', '', regex=True)
 
-    # coerce the resultant output to csv format
-    df['Installs'] = pd.to_numeric(df['Installs'], errors='coerce')
+        # coerce the resultant output to csv format
+        df['Installs'] = pd.to_numeric(df['Installs'], errors='coerce')
 
-    # clean the Android Ver column 
-    df['Android Ver'] = df['Android Ver'].str.replace(' and up', '')
+        # clean the Android Ver column 
+        df['Android Ver'] = df['Android Ver'].str.replace(' and up', '')
 
-    # drop NaN/NULL values
-    df.dropna()
+        # drop NaN/NULL values
+        df.dropna()
 
-    # create the placeholder for the filename
-    desired_file_name = 'cleaned_googleplaystore.csv'
+        # create the placeholder for the filename
+        desired_file_name = 'cleaned_googleplaystore.csv'
 
-    # save the df to the csv file
-    df.to_csv(desired_file_name, index=False)
+        # save the df to the csv file
+        df.to_csv(desired_file_name, index=False)
 
-    print("Successful operation")
+        logging.info("Data cleaning and preparation completed successfully.")
+        return df, "Data cleaning and preparation completed successfully."
 
-    return "Data cleaning and preparation completed successfully."
+    except Exception as e:
+        logging.error(f"Error during data preparation: {e}")
+        return None, f"Error during data preparation: {e}"
+
+# logging
+logging.basicConfig(level=logging.INFO) 
+
+# Example usage:
+result_df, result_message = data_prep(r"C:\Users\DELL\Desktop\Appdataprocess\googleplaystore.csv")
+
+print("result_message")
